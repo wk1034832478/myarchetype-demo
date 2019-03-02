b505d3bb318c1077630f5ead33479a41ef35600d
wangkai <1034832478@qq.com>
2019-03-02 11:13:19 +0800

    update

--- src/test/java/home/hyywk/top/AppTest.java
+++ src/test/java/home/hyywk/top/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +18,14 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    public void test1() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream( "/home/wangkai/aa.txt" );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
